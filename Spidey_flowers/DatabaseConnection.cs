01a912f8b3a2df6f68f584dc292aac44acae4a43
Dhiren Audich <dhirenaudich@gmail.com>
2014-07-22 23:33:41 -0400

    Added the main page, starting on migrating db class

---
+++ Spidey_flowers/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; 
+
+namespace Spidey_flowers
+{
+	private string sql_string;
+	private string strCon;
+
+	public string Sql{
+	    set{ sql_string = value; }
+	}
+
+    public string connectionString{
+	    set{ strCon = value; }
+	}
+
+}
