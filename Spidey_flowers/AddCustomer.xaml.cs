5e0a3fc4a9bc490b87051cb1ee15d8d0d299cfab
Dhiren Audich <dhirenaudich@gmail.com>
2014-07-24 20:20:13 -0400

    Cleaning up add a new customer

--- Spidey_flowers/AddCustomer.xaml.cs
+++ Spidey_flowers/AddCustomer.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
 using System.Data.SqlClient;
 
 namespace Spidey_flowers
@@ -67,7 +66,7 @@ namespace Spidey_flowers
             try
             {
                 _dbConnect.Sql = query;
-                //Console.WriteLine("wrote :: " + _dbConnect.insertIntoDB());
+
                 SqlDataReader reader = _dbConnect.run();
                 reader.Close(); //Need to close the reader as the DatabaseConnection.run method returns a reader.
 
